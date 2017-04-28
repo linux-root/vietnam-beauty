82af71fd71493751d1133af08e33a5084bb33bc0
Kurro <superusr.root@gmail.com>
2017-04-28 11:39:47 +0700

    components

--- src/components/main/Home.vue
+++ src/components/main/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'home',
+  data () {
+    return {
+
+    }
+  }
 }
 </script>
 
+
+
+
 <style>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Page from '../components/Page.vue'
+import Mountain from '../components/Mountain.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  history: true,
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Page
+    },
+    {
+      path: '/mountain',
+      name: 'Mountain',
+      component: Mountain
     }
   ]
 })
