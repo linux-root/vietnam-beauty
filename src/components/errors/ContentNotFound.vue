0b331c20148939c76e844cc84a3ee98dc230dadd
Kurro <superusr.root@gmail.com>
2017-04-28 11:31:59 +0700

    components

---
+++ src/components/errors/ContentNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content bg-white text-center overflow-hidden">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-3">
+        <h1 class="font-s128 font-w300 text-city animated flipInX">404</h1>
+        <h2 class="h3 font-w300 push-50 animated fadeInUp">We are sorry but the page you are looking for was not found..</h2>
+        <form class="form-horizontal push-50" action="base_pages_search.html" method="post">
+          <div class="form-group">
+            <div class="col-sm-6 col-sm-offset-3">
+              <div class="input-group input-group-lg">
+                <input class="form-control" type="text" placeholder="Search application..">
+                <div class="input-group-btn">
+                  <button class="btn btn-default"><i class="fa fa-search"/></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ContentNotFound'
+  }
+</script>
---
+++ src/components/extends/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 ">
+          <div class="font-w500 text-gray-darker animated text-city">
+            <img class="img-avatar img-avatar96 img-avatar-thumb" src="/static/img/avatars/kurro.jpg">
+            © 2017 Designed and Developed by <a href="https://fb.me/mr.kurro">DinhBaoKhanh</a></div>
+        </div>
+        <div class="col-md-4">
+          <p class="text-right social"><i class="typcn typcn-social-facebook-circular"></i>
+            <i class="typcn typcn-social-twitter-circular"></i>
+            <i class="typcn typcn-social-github-circular"></i></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'web-footer'
+  }
+</script>
